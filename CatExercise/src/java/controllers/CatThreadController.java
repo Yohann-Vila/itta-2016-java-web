e768e59f412902ceb45f6b882951a7d15bd3dbdb
lys <lysergic@bluewin.ch>
2016-02-25 12:10:14 +0100

    Update catThreadController

--- CatExercise/src/java/controllers/CatThreadController.java
+++ CatExercise/src/java/controllers/CatThreadController.java
@@ -1,5 +1,20 @@
 package controllers;
 
+import business.*;
+
 public class CatThreadController {
+
+    private ICatThreadDAO catThreadDAO = DAOFactory.getInstanceOfCatThread();
+
+    boolean createThread(String title, String uriPhoto, User user) {
+        CatThread c = new CatThread(user.getLogin(), title, uriPhoto);
+        boolean b = false;
+        try {
+            b = catThreadDAO.create(c);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return b;
+    }
     
 }
