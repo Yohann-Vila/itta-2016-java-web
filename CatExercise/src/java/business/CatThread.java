5a60a6095e28197ff8405ef1b221f3880defa5ee
Yohann-Vila <yohann.vila@signatys.com>
2016-02-25 10:33:48 +0100

    Implémentation CatThread

--- CatExercise/src/java/business/CatThread.java
+++ CatExercise/src/java/business/CatThread.java
@@ -8,13 +8,52 @@ import java.util.Date;
  * @author Yohann
  */
 public class CatThread {
-    private int catThreadId;
-    private String login;
-    private String titre;
-    private String uriPhoto;
-    private boolean isDeleted;
-    private Date creationDate;
+    private final int catThreadId;
+    private final String login;
+    private final String titre;
+    private final String uriPhoto;
+    private boolean deleted = false;
+    private final Date creationDate = new Date();
+    Collection<Comment> comments;
     
+    public int getCatThreadId() {
+        return catThreadId;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public String getUriPhoto() {
+        return uriPhoto;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public Collection<Comment> getComments() {
+        return comments;
+    }
     
+
     
+    public CatThread(int catThreadId, String login, String titre, String uriPhoto) {
+        this.catThreadId = catThreadId;
+        this.login = login;
+        this.titre = titre;
+        this.uriPhoto = uriPhoto;
+    }
+
+    public void deleteThread() {
+        this.deleted = true;
+    }
 }
