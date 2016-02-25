e00cfd993c8d2df466f1996ea25ef50ef2784f3a
Yohann-Vila <yohann.vila@signatys.com>
2016-02-25 10:27:29 +0100

    Début implémentation CatThread

--- CatExercise/src/java/business/CatThread.java
+++ CatExercise/src/java/business/CatThread.java
@@ -1,9 +1,20 @@
 package business;
 
+import java.util.Collection;
+import java.util.Date;
+
 /**
  *
  * @author Yohann
  */
 public class CatThread {
+    private int catThreadId;
+    private String login;
+    private String titre;
+    private String uriPhoto;
+    private boolean isDeleted;
+    private Date creationDate;
+    
+    
     
 }
