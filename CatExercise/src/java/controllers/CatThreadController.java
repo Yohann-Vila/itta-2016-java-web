118f1693b56e5563a1652c96439ab77cd3aabb2b
lys <lysergic@bluewin.ch>
2016-02-25 13:37:29 +0100

    Update catThreadController

--- CatExercise/src/java/controllers/CatThreadController.java
+++ CatExercise/src/java/controllers/CatThreadController.java
@@ -1,12 +1,32 @@
 package controllers;
 
-import business.*;
+import business.CatThread;
+import business.DAOFactory;
+import business.ICatThreadDAO;
+import business.User;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class CatThreadController {
+@WebServlet(name = "CatThreadController", urlPatterns = {"/thread"})
+public class CatThreadController extends HttpServlet {
 
-    private ICatThreadDAO catThreadDAO = DAOFactory.getInstanceOfCatThread();
+    private final ICatThreadDAO catThreadDAO = DAOFactory.getInstanceOfCatThread();
 
-    boolean createThread(String title, String uriPhoto, User user) {
+
+    /*Overide*/
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("OK");
+        //createThread(title, uriPhoto, user)
+    }
+
+    /*Method Private*/
+    private boolean createThread(String title, String uriPhoto, User user) {
         CatThread c = new CatThread(user.getLogin(), title, uriPhoto);
         boolean b = false;
         try {
@@ -16,5 +36,4 @@ public class CatThreadController {
         }
         return b;
     }
-    
 }
