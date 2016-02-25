074cddb318e47f6d980a1127fbb87a15f9b05933
lys <lysergic@bluewin.ch>
2016-02-25 15:11:34 +0100

    Pagination ok

--- CatExercise/src/java/controllers/CatThreadController.java
+++ CatExercise/src/java/controllers/CatThreadController.java
@@ -2,17 +2,17 @@ package controllers;
 
 import business.*;
 import java.io.*;
+import java.util.*;
 import javax.servlet.*;
 import javax.servlet.annotation.*;
 import javax.servlet.http.*;
-
 
 @WebServlet(name = "CatThreadController", urlPatterns = {"/thread"})
 public class CatThreadController extends HttpServlet {
 
     private final ICatThreadDAO catThreadDAO = DAOFactory.getInstanceOfCatThread();
     private final IUserDAO userDAO = DAOFactory.getInstanceOfUser();
-
+    private int nbByPage = 10;
 
 
     /*Overide*/
@@ -20,9 +20,20 @@ public class CatThreadController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
         out.println("OK");
+        int currentPage = 1;
+        try {
+            currentPage = Integer.parseInt(req.getParameter("page"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
         out.println(createThread("Test", "/photo/chat1.png", userDAO.find("toto")));
+        out.println("Test PAGINATION : "+currentPage);
+        
+        for (CatThread catThread : getThreadByPages(currentPage)) {
+            out.println(catThread.getTitre());
+        }
+        out.print("Fin Test PAGINATION");
         out.println("Fin");
-
     }
 
     /*Method Private*/
@@ -36,5 +47,26 @@ public class CatThreadController extends HttpServlet {
         }
         return b;
     }
-    
+
+    private LinkedHashSet<CatThread> getThreadByPages(int currentPage) {
+        LinkedHashSet<CatThread> outSet = new LinkedHashSet<>();
+        int maxValue = currentPage * nbByPage;
+        int stratValue = (currentPage - 1) * nbByPage;
+        int i = 0;
+        Iterator<CatThread> it = catThreadDAO.getAll().iterator();
+        while (it.hasNext() && stratValue < maxValue) {
+            if (i >= stratValue) {
+                outSet.add(it.next());
+                stratValue++;
+            }else{
+                it.next();
+            }
+            i++;
+        }
+        return outSet;
+    }
+
+    private void setThreadPages(int nbByPage) {
+        this.nbByPage = nbByPage;
+    }
 }
