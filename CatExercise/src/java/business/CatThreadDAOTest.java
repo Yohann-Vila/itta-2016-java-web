2a7dfa8e109bea445488cef8fb0829a56b480616
Yohann-Vila <yohann.vila@signatys.com>
2016-02-25 11:21:18 +0100

    Stubs for DAO Implementation test classes

---
+++ CatExercise/src/java/business/CatThreadDAOTest.java
@@ -0,0 +1,41 @@
+package business;
+
+import java.util.Collection;
+
+/**
+ *
+ * @author Yohann
+ */
+public class CatThreadDAOTest implements ICatThreadDAO {
+    
+    @Override
+    public Collection<CatThread> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Collection<CatThread> findByLogin(String login) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Collection<CatThread> findByTitle(String partialTitle) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean Modify(CatThread catThread) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean Create(CatThread catThread) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public CatThread findByID(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
