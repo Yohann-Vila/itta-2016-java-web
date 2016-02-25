04cd9455f1962323c470bdb0a4289983b4debb20
lys <lysergic@bluewin.ch>
2016-02-25 10:23:51 +0100

    Creation class comment & interface

--- CatExercise/src/java/business/Comment.java
+++ CatExercise/src/java/business/Comment.java
@@ -1,3 +1,56 @@
+package business;
+
+import java.util.Date;
+
 public class Comment {
+    private int commentID;
+    private int threadID;
+    private String login;
+    private Date creationDate;
+    private boolean isDeleted;
+
+    public Comment(int threadID, String login) {
+        this.threadID = threadID;
+        this.login = login;
+    }
     
+    public int getCommentID() {
+        return commentID;
+    }
+
+    public void setCommentID(int commentID) {
+        this.commentID = commentID;
+    }
+
+    public int getThreadID() {
+        return threadID;
+    }
+
+    public void setThreadID(int threadID) {
+        this.threadID = threadID;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public boolean isIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
 }
